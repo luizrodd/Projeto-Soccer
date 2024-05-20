10cfb0e03c4ec0ed958e967144f5da1d0650abe2
luizrodd <luizbr080604@gmail.com>
2024-05-20 11:14:56 -0300

    firebase config

--- Web/src/app/app.config.ts
+++ Web/src/app/app.config.ts
@@ -1,9 +1,26 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4-MkFL1hCxCRgcsIPsJMSC6_s_DNOPkE",
+  authDomain: "isport-team.firebaseapp.com",
+  projectId: "isport-team",
+  storageBucket: "isport-team.appspot.com",
+  messagingSenderId: "880728253187",
+  appId: "1:880728253187:web:6eca116b8f4d011b697f36"
+};
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
+  
 };
